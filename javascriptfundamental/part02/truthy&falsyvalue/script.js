0df84ade96879bcd8b986d42fdc5227be8362289
Megha Khakha <meghakhakha148@gmail.com>
2025-06-01 22:47:57 +0530

    truthy-falsy

---
+++ javascriptfundamental/part02/truthy&falsyvalue/script.js
@@ -0,0 +1,21 @@
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean(0));
+console.log(Boolean('Jonas'));
+console.log(Boolean(''));
+
+
+const money = 100;
+if(money {
+    console.log("Do not spend it all ;)");
+}) else {
+    console.log('you should get a job');
+}
+
+
+let height = 0
+if (height) {
+    console.log('height is defined');
+}else {
+    console.log('height is undefined');
+}
