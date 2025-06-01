a8597782dd8b865c2bd8bcb9ccf6a3614ec23b39
Megha Khakha <meghakhakha148@gmail.com>
2025-06-01 16:03:27 +0530

    ongoing

---
+++ javascriptfundamental/basicOperators/script.js
@@ -0,0 +1,25 @@
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas , ageSarah);
+
+
+console.log(ageJonas * 2 , ageJonas / 10 , 2 ** 3);
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName +' '+ lastName);
+
+// ASINGNMENT OPERATORS
+let x = 10 + 5;
+x += 10; // x = x + 10
+x *= 4;  // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+// COMPARISION OPERATORS
+console.log(ageJonas > ageSarah); 
+console.log(ageJonas >= 18); 
+console.log(ageJonas > ageSarah); 
+console.log(ageJonas > ageSarah); 
