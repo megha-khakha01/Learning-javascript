02a6e4a9a368cb1e7deed940980aa95aceacdf12
Megha Khakha <meghakhakha148@gmail.com>
2025-06-01 15:28:37 +0530

    progress

---
+++ javascriptfundamental/datatype/script.js
@@ -0,0 +1,10 @@
+ let javascriptIsFun  = true;
+ console.log(javascriptIsFun);
+
+
+ console.log(typeof true);
+ console.log(typeof javascriptIsFun);
+ console.log(typeof 23);
+ console.log(typeof 'Jonas');
+//  to change the value of the variable
+javascriptIsFun = 'YES!';
