5b00756dcc670c414fbfd6f33437c75d4dded9a4
Megha Khakha <meghakhakha148@gmail.com>
2025-06-01 23:44:59 +0530

    truthy-falsy

---
+++ javascriptfundamental/part02/codingchallenge#3/script.js
@@ -0,0 +1,13 @@
+const scoreDolphins = (97 + 112 + 80 ) / 3;
+const scoreKoalas = (109 + 95 + 50 ) / 3;
+console.log(scoreDolphins ' scoreKoalas');
+
+if (scoreDolphins> scoreKoalas && scoreDolphins >=100){
+    console.log('Dolphins win the trophy');
+}else if (scorekoalas > scoredolphins && scorekoalas >=100)
+    console.log('koalas win the trophy');
+}else if (scoredolphins === scorekoalas && scoredolphins>=100 && scoreKoalas>=100)
+    console.log('Both win the trophy');
+}else {
+    console.log('no one wins the trophy');
+}
