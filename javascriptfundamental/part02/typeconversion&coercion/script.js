012ba0aec9ae2e8a82b9bd8ee5cfc98e7e3785fb
Megha Khakha <meghakhakha148@gmail.com>
2025-06-01 22:27:33 +0530

    conversio&coercion

---
+++ javascriptfundamental/part02/typeconversion&coercion/script.js
@@ -0,0 +1,16 @@
+//type conversion
+const inputYear = '1991';
+console.log(Number(inputYear));
+console.log(Number(inputYear) + 18);
+
+
+//type coertion
+console.log('I am' + 23 + 'years old');
+console.log('23'-'10'-3);
+console.log('23'+'10'+3);
+
+
+
+let n='1'+1;
+n = n - 1;
+console.log(n);
