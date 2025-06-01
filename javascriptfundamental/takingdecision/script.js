66bd4981ff87c3430b390a9bec9b0937c47c273b
Megha Khakha <meghakhakha148@gmail.com>
2025-06-01 19:58:38 +0530

    decision making

---
+++ javascriptfundamental/takingdecision/script.js
@@ -0,0 +1,16 @@
+/*const age = 19;
+const isOldEnough = age >= 18;
+
+if (isOldEnough) { 
+    console.log('Sarah can start driving license🚗');
+}*/
+
+const age = 15;
+
+if (age >= 18) {
+    console.log('Sarah can start driving license');
+}
+else{
+    const yearsLeft = 18 - age;
+    console.log('Sarah is too young. Wait another $ {yearsLeft} years:)'};
+}
