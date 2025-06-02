92f69383c29d47f2da1bda7d1ca0a2fae5891697
Megha Khakha <meghakhakha148@gmail.com>
2025-06-02 10:11:18 +0530

    part03

--- javascriptfundamental/part03/script.js
+++ javascriptfundamental/part03/script.js
@@ -69,7 +69,7 @@ console.log(`I like to drink ${_Age >= 18 ? 'wine' : 'water'}`);
 //coding challenge#4
 
 /*
-const bill = 430
+const bill = 430;
 const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
 console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 
