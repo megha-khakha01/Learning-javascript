6896e59b66458436e1ca58e5e9ad6949f6292780
Megha Khakha <meghakhakha148@gmail.com>
2025-06-02 10:39:20 +0530

    fundamental02

--- javascriptfundamental02/script.js
+++ javascriptfundamental02/script.js
@@ -19,7 +19,7 @@ console.log('My name is shivi');
  }
 /*
 //calling / running / invoking function
- logger();
+ /*logger();
  logger();
  logger();
 
@@ -29,11 +29,11 @@ function fruitProcessor(apples, oranges) {
     return juice;
  }
 
-// // const applejuice = fruitProcessor(5,0);
-// // console.log(applejuice);
+ const applejuice = fruitProcessor(5,0);
+ console.log(applejuice);
 
-// // const appleOrange = fruitProcessor(10,5);
-// // console.log(appleOrange);
+ const appleOrangeJuice = fruitProcessor(2,4);
+console.log(appleOrangeJuice);*/
 
 // // // ********FUNCTION DECLARATION********
 
