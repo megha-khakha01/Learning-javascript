1c78c3dfdc0fd65eb1206a9ba07dd0a20223e590
Megha Khakha <meghakhakha148@gmail.com>
2025-06-02 10:09:42 +0530

    03

---
+++ javascriptfundamental/part03/script.js
@@ -0,0 +1,76 @@
+/*switch (day) {
+  case 'monday': // day === 'monday'
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code example');
+    break;
+  case 'friday':
+    console.log('Record videos');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
+    break;
+  default:
+    console.log('Not a valid day!');
+} 
+
+if (day === 'monday') {
+  console.log('Plan course schedule');
+  console.log('Go to coding meetup');
+} else if (day === 'tuesday') {
+  console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday') {
+  console.log('Write code examples');
+} else if (day === 'friday') {
+  console.log('Record videos');
+} else if (day === 'saturday' || day === 'sunday') {
+  console.log('Enjoy the weekend :D');
+} else {
+  console.log('Not a valid day!');
+}
+*/
+
+//statements and expressions
+/*
+3 + 4
+1991
+true && false && !false
+
+if (23>10) {
+    const str = '23 is bigger';
+}
+const me ='Megha';
+console.log(`I'm ${2037-1991} years old ${me}`);
+*/
+//The conditional (Ternary)Operators
+
+/*const drink = Age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if (_Age >= 18) {
+  drink2 = 'wine';
+} else {
+  drink2 = 'water';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${_Age >= 18 ? 'wine' : 'water'}`);
+
+*/
+
+//coding challenge#4
+
+/*
+const bill = 430;
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+*/
