e7d202dbf137e616bde9d3f2820dd2b0ce15c13e
Megha Khakha <meghakhakha148@gmail.com>
2025-06-02 11:29:25 +0530

    coding challeenge

--- javascriptfundamental02/script.js
+++ javascriptfundamental02/script.js
@@ -133,12 +133,12 @@ yearsUntilRetirement(1950, 'Megha');
 
 // Coding challenge
 
-const calcAverage = (a, b, c) => (a + b + c) / 3;
-console.log(calcAverage(3, 4, 5));
+/*const calcAverage = (a, b, c) => (a + b + c) / 3;
+console.log(calcAverage(3, 4, 5));*/
 
 // Test 1
 
-let scoreDolphins = calcAverage(44, 23, 71);
+/*let scoreDolphins = calcAverage(44, 23, 71);
 let scoreKoalas = calcAverage(65, 54, 49);
 console.log(scoreDolphins, scoreKoalas);
 
@@ -153,11 +153,11 @@ const checkWinner = function (avgDolphins, avgKoalas) {
 }
 checkWinner(scoreDolphins, scoreKoalas);
 
-checkWinner(543, 111);
+checkWinner(543, 111);*/
 
 
 //Test 2
-scoreDolphins = calcAverage(85 , 54, 41);
+/*scoreDolphins = calcAverage(85 , 54, 41);
 scoreKoalas = calcAverage(23 , 34 , 27);
 console.log(scoreDolphins, scoreKoalas);
-checkWinner(scoreDolphins, scoreKoalas);
+checkWinner(scoreDolphins, scoreKoalas);*/
