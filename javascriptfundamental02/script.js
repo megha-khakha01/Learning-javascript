b57f50fe1762a12bf48915d0ba8b425b45e3295f
Megha Khakha <meghakhakha148@gmail.com>
2025-06-04 13:11:22 +0530

    coding challenge

--- javascriptfundamental02/script.js
+++ javascriptfundamental02/script.js
@@ -353,6 +353,43 @@ checkWinner(scoreDolphins, scoreKoalas);*/
       //     dice = Math.trunc(Math.random() * 6) + 1;
       //     if (dice === 6) console.log ('Loop is about to end...');
       //   }
+
+
+      //  Coding Challenge  //
+
+      // const calcTip = function (bill) {
+      //   return bill >= 50 && bill <=300 ? bill * 0.15 : bil * 0.2;
+      // }
+
+      // const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52]
+      // const tips = [];
+      // const totals = [];
+
+      // for (let i = 0; i < bills.length; i++) {
+      //   const tip = calcTip(bills[i]);
+      //   tips.push(tip);
+      //   totals.push(tip+bills[i]);
+      // }
+
+      // console.log(bills , tips , totals);
+
+      // const calcAverage = functions (arr) {
+      //   let sum = 0
+      //   for (let i = 0; i < arr.length; i++){
+      //      // sum= summ + arr [i];
+      //      sum += arr[i];
+        
+      //   }
+      //   return sum / arr . length;
+      // }
+
+      // console.log(calcAverage[2 , 3 , 7]);
+      // console.log(calcAverage(totals));
+      // console.log(calcAverage(tips));
+      
+
+
+
       
 
 
