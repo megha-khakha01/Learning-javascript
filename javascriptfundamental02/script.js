909c20a9f43aa5fe67a9b61f551191ac01f93157
Megha Khakha <meghakhakha148@gmail.com>
2025-06-04 12:59:29 +0530

    while loop

--- javascriptfundamental02/script.js
+++ javascriptfundamental02/script.js
@@ -333,6 +333,30 @@ checkWinner(scoreDolphins, scoreKoalas);*/
       // }
 
 
+      //  While Loop  //
+
+      //for (let rep = 1; rep <= 10 ; rep ++){
+        //console.log(Lifting weights repetition ${rep}');
+      // }
+
+      // let rep = 1;
+      // while (rep <=10) {
+      //  // console.log ('WHILE: lifting weights repetition'${rep}');
+      //     rep++;
+      // }
+
+      // let dice = Maths.trunc(Math.random()*6 ) + 1;
+      // console.log(dice);
+
+      //   while(dice !== 6) {
+      //     console.log('You rolled a ${dice}');
+      //     dice = Math.trunc(Math.random() * 6) + 1;
+      //     if (dice === 6) console.log ('Loop is about to end...');
+      //   }
+      
+
+
+
 
 
 
