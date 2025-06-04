6ae27f8c9a3ad571d8d008ecf3ad0e6fec044f50
Megha Khakha <meghakhakha148@gmail.com>
2025-06-04 12:48:12 +0530

    looping backward and loops in loops

--- javascriptfundamental02/script.js
+++ javascriptfundamental02/script.js
@@ -306,6 +306,31 @@ checkWinner(scoreDolphins, scoreKoalas);*/
       //   console.log(jonas[i] , typeof jonas[i]);
       // }
 
+      // Looping Backward And Loops In Loops  //
+
+      // const jonas = [
+      //   'Jonas'
+      //   'Schmedtmann',
+      //   2037 - 1991,
+      //   'teacher'
+      //   ['Michael', 'Peter', 'Steven']
+      //   true
+      // ];
+
+      // // 0, 1, ..., 4
+      // // 4, 3, ..., 0
+
+      // for (let i = jonas.length - 1 ; i >= 0; i--){
+      //   console.log(i, jonas[i]);
+      // }
+
+      // for (let exercise = 1; exercise < 4; exercise++){
+      //   console.log(-------Starting exercise ${exercise}');
+
+      //     for(let rep = 1 ; rep < 6; rep++){
+      //       console.log('Lifting weight repetition ${rep}');
+      //     }
+      // }
 
 
 
