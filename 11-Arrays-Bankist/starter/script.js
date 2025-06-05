3e955bd247ac08b818e256e98cf8974bb08be542
Megha Khakha <meghakhakha148@gmail.com>
2025-06-05 21:28:18 +0530

    arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -146,3 +146,21 @@
 // //0; function(450)
 // //0; function(400)
 // //...
+
+
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function(value, key, map){
+  console.log(''${key}: ${value}');
+});
+
+//Set
+const currenciesUnique = new Set([ 'USD', 'GBP', 'USD', 'EUR', 'EUR']);
+console.log(currenciesUnique);
+currenciesUnique.forEach(function(value, _, map){
+  console.log('{$key}: ${value}');
+});
