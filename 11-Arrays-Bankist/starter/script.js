ea890ac1eb16e9279fe52f23d4e7be2d1a8859b9
Megha Khakha <meghakhakha148@gmail.com>
2025-06-05 21:20:28 +0530

    arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -121,3 +121,28 @@
 
 // console.log('shivi' .at(0));
 // console.log('shivi' .at(-1));
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+//for (const movement of movements) {
+for (const [i, movement] of movements.entries()){
+  if (movement > 0) {
+    console.log ('Movement ${i + 1}:You deposited ${movement}');
+  }else {
+    console.log('Movement ${i + 1}: You withdrew ${Math.abs(movment)}');
+  }
+}
+
+movements.forEach('---FOREACH---');
+movements.forEach(function(mov, i, arr){
+ if (mov > 0) {
+    console.log ('Movement ${i + 1}:You deposited ${mov}');
+  }else {
+    console.log('Movement ${i + 1}: You withdrew ${Math.abs(mov)}');
+  }
+});
+
+//0; function(200)
+//0; function(450)
+//0; function(400)
+//...
