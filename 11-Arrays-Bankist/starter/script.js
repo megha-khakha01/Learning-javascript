2b30a776e9d065caf296d35184800f16a4073e92
Megha Khakha <meghakhakha148@gmail.com>
2025-06-05 23:45:45 +0530

    coding challenge

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -207,6 +207,7 @@ const checkDogs = function (dogsJulia , dogsKate){
         console.log ('Dog number ${i+1} is still a puppy');
       }
     })
-  )
-}
-checkDogs([3, 5, 2 , 12, 7] , [4 , 1, 15 ,8 ,3]);
+  );
+};
+//checkDogs([3, 5, 2 , 12, 7] , [4 , 1, 15 ,8 ,3]);
+checkDogs ([9,16,6,8,3], [10,5,6,1,4]);
