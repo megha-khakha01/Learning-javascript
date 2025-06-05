5ba743b3567bbc01c9c1d5bb5b91260f1481addc
Megha Khakha <meghakhakha148@gmail.com>
2025-06-05 20:57:50 +0530

    arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -108,3 +108,16 @@
 // console.log(letters.join( ' - ' ));
 
 
+const arr = [23,11,64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+
+//getting last array element
+
+comsole.log(arr[arr.length-1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log('shivi' .at(0));
+console.log('shivi' .at(-1));
