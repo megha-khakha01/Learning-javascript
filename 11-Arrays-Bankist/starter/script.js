99d3308de3edc2edc191ba77ffcb0e7477963e5d
Megha Khakha <meghakhakha148@gmail.com>
2025-06-05 22:45:53 +0530

    arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,29 @@
 // const inputCloseUsername = document.querySelector('.form__input--user');
 // const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function(movements){
+  movements.forEach(function (mov , i) {
+    const type = mov > 0 ? 'deposite' : 'withdraw'
+  });
+  };
+  displayMovements(account1.movements);{
+    const html ='
+     <div class="movements__row">
+          <div class="movements__type movements__type--${type}">$
+          i + 1
+          } ${type} </div>
+          <div class="movements__value">${mov}</div>
+        </div>
+    ';
+
+      containerMovements .insertAdjacentHTML('afterbegin');
+
+
+
+  });
+};
+    displayMovements(account1.movement);
+
 // /////////////////////////////////////////////////
 // /////////////////////////////////////////////////
 // // LECTURES
