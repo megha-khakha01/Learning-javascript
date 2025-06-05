8fd5a0ae1e0081235ae40b9fca2a6c8fca65e097
Megha Khakha <meghakhakha148@gmail.com>
2025-06-05 23:46:35 +0530

    part 1

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -195,19 +195,19 @@
 
 // CODING CHALLENGE //
 
-const checkDogs = function (dogsJulia , dogsKate){
-  const dogsJuliaCorrected = dogsJulia.slice(;
-    dogsJuliaCorrected.splice(0, 1);
-    dogsJuliaCorrected.splice(-2);
+// const checkDogs = function (dogsJulia , dogsKate){
+//   const dogsJuliaCorrected = dogsJulia.slice(;
+//     dogsJuliaCorrected.splice(0, 1);
+//     dogsJuliaCorrected.splice(-2);
 
-    dogsJulia.forEach(function(dog, i){
-      if (dog>=3){
-        console.log('Dog number ${i + 1} is an adult, and is ${dog}years old');
-      }else {
-        console.log ('Dog number ${i+1} is still a puppy');
-      }
-    })
-  );
-};
-//checkDogs([3, 5, 2 , 12, 7] , [4 , 1, 15 ,8 ,3]);
-checkDogs ([9,16,6,8,3], [10,5,6,1,4]);
+//     dogsJulia.forEach(function(dog, i){
+//       if (dog>=3){
+//         console.log('Dog number ${i + 1} is an adult, and is ${dog}years old');
+//       }else {
+//         console.log ('Dog number ${i+1} is still a puppy');
+//       }
+//     })
+//   );
+// };
+// //checkDogs([3, 5, 2 , 12, 7] , [4 , 1, 15 ,8 ,3]);
+// checkDogs ([9,16,6,8,3], [10,5,6,1,4]);
