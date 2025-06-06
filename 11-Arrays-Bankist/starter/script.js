9fcbbc6032e39574e4f05b33a0a3185d73ab8b72
Megha Khakha <meghakhakha148@gmail.com>
2025-06-06 19:14:00 +0530

    part02

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -239,3 +239,16 @@
      
 //  };
 // console.log(movementsDescriptions);
+
+const deposits = movements.filters(function (mov){
+return mov > 0;
+} ) ;
+console.log(movements);
+console.log(deposits);
+
+const depositsFor = [];
+for (const mov of movements) if (mov > 0) depositsFor.push (mov);
+console.log (depositsFor);
+
+const withdraw = movements.filter(mov => mov < 0);
+console.log(withdrawals);
