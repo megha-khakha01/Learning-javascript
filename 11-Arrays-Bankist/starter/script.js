9b76483290142bc5f728581fc6217e841e36bfee
Megha Khakha <meghakhakha148@gmail.com>
2025-06-06 19:42:07 +0530

    part02

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -272,11 +272,11 @@
 // console.log(balance2);
 
 //Maximum value 
-const max = movements.reduce((acc , mov) => {
-    if(acc > mov) return acc;
-    else return mov;
-}, movements[0]);
-console.log(max);
+// const max = movements.reduce((acc , mov) => {
+//     if(acc > mov) return acc;
+//     else return mov;
+// }, movements[0]);
+// console.log(max);
 
 
 //Coding Challenge
