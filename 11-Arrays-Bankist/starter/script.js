d8dda7c198842a90ffbb71cca584399c79e19299
Megha Khakha <meghakhakha148@gmail.com>
2025-06-06 19:42:25 +0530

    part02

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -281,11 +281,11 @@
 
 //Coding Challenge
 
-const calcAverageHumanAge = function (ages) {
-  const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
-  const adults = humanAges.filter(age => age >= 18);
-  console.log(humanAges);
-  console.log(adults);
+// const calcAverageHumanAge = function (ages) {
+//   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+//   const adults = humanAges.filter(age => age >= 18);
+//   console.log(humanAges);
+//   console.log(adults);
 
   // const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
 
