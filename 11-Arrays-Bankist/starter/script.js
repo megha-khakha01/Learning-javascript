d1a3eea88c29066dc69cdf54b7e0cf7f89deadfd
Megha Khakha <meghakhakha148@gmail.com>
2025-06-07 13:44:38 +0530

    part03

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -152,6 +152,27 @@
 
 // });
 
+btnTransfer.addEventListener('click', function (e){
+  e.preventDefault();
+  const amount = Number(inputTransferAmount.value);
+  const receiverAcc = accounts.find(
+    acc => acc.username === inputTransferTo.value
+  );
+inputTransferAmount.value = inputTransferTo = '';
+  if(amount > 0 &&
+    receiverAcc &&
+    currentAccount.balance >= amount &&
+    receiverAcc?. username != currentAccount.username
+  ){
+    // Doing the transfer
+    currentAccount.movements.push(-amount);
+    receiverAcc.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+});
+
 // /////////////////////////////////////////////////
 // /////////////////////////////////////////////////
 // // LECTURES
