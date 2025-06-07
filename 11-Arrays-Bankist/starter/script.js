c6c721db05d00f59d5521778d2b4a613e73f9729
Megha Khakha <meghakhakha148@gmail.com>
2025-06-07 16:33:23 +0530

    part02

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -894,12 +894,12 @@ console.log(new Date(3 * 24 * 60 * 60 * 1000));
 
 
 // Operations With Date
-const f = new Date(2037, 10, 19, 15, 23);
-console.log(+future);
+// const f = new Date(2037, 10, 19, 15, 23);
+// console.log(+future);
 
-const calcDaysPassed = (date1, date2) =>
-  Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
+// const calcDaysPassed = (date1, date2) =>
+//   Math.abs(date2 - date1) / (1000 * 60 * 60 * 24);
 
-const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
-console.log(days1);
+// const days1 = calcDaysPassed(new Date(2037, 3, 4), new Date(2037, 3, 14));
+// console.log(days1);
 
