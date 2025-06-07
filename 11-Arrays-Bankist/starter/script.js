5d067ef22dfade5471dbb60b302005065a2f013d
Megha Khakha <meghakhakha148@gmail.com>
2025-06-07 15:51:29 +0530

    part05

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -578,3 +578,80 @@
 
 //   const movementsUI2 = [...document.querySelectorAll('.movements_value')];
 // });
+
+
+// Array Methods Practice
+
+
+// 1.
+const bankDepositSum = accounts
+ .flatMap(acc => acc. movements)
+ .filter(mov => mov > 0)
+ .reduce((sum, curr) => sum + curr, 0);
+
+ console.log(bankDepositSum);
+
+
+ // 2.
+ // const numDeposits1000 = accounts
+ // .flatMap(acc => acc.movements)
+ // .filter(mov => mov >= 1000).length;
+
+ const numDeposits1000 = accounts.flatMap(acc => acc.movements)
+ // .reduce((count, curl) => ( cur >= 1000 ? count + 1
+ //: count). 0);
+ .reduce((count, cur) => (cur >= 1000 ? count++ :
+  count), 0);
+ 
+
+  console.log(numDeposits1000);
+
+
+
+  // prefixed ++ operator
+  let a = 10;
+  console.log(++a);
+  console.log(a);
+
+
+  // 3.
+  //const sums = accounts
+  //.flatMap(acc => acc.movements). reduce(())
+const {deposits, withdrawals } = accounts 
+.flatMap(acc => acc.movements)
+.reduce (
+(sums, cur) => 
+  // curl > 0 ? (sums.deposits += curl) : (sums.
+  withdrawals +=cur);
+  sum[cur >0 ? 'deposits' : 'withdrawals'] +=
+  cur;
+  return sums;
+},
+{deposits: 0, withdrawals: 0}
+);
+
+console.log(deposits, withdrawals);
+
+
+
+// 4.
+// this is a nice title --> This Is a Nice Title
+const convertTitleCase = function(title) {
+  const capitalize = str => str [0].toUpperCase() +
+  str.slice(1);
+
+  const exceptions = ['a', 'an', 'and', 'the', 'but', 
+    'or', 'on', 'in','with'];
+
+    const titleCase = title 
+    .toLowerCase()
+    .split('')
+    .map(word => (expections.include(word) ? word : 
+  capitzalize(word)))
+  .join('');
+  return titleCase;
+};
+
+console.log(convertTitleCase ('this is a nice titke'));
+console.log(convertTitleCase ('this ia a Long title but not too long'));
+console.log(convertTitleCase ('and here is another title with an EXAMPLE'));
