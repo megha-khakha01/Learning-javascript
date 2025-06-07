56d3f01ce73c51fd57bd8bf70b85a46d40f67730
Megha Khakha <meghakhakha148@gmail.com>
2025-06-07 15:38:57 +0530

    part04

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -546,3 +546,35 @@
 // });
 
 // console.log(movements);
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+
+//Emprty arrays + fill method
+const x = new Array(7);
+console.log(x);
+// console.log(x.map(() => 5));
+x.fill(1,3,5);
+x.fill(1, 3, 5);
+console.log(x);
+
+arr.fill(23, 4, 6);
+console.log(arr);
+
+//Array.from
+const y = Array.from({length: 7}, () => 1);
+console.log(y);
+
+const z = Array.from({ length: 7},() => 1 );
+console.log(z);
+
+lookBalance.addEventListner('click', function (){
+  const movementsUI =Array.from(
+    document.querySelectorAll('.movements_value'),
+    el => Number(el.textContent.replace('',''))
+  );
+
+  console.log(movementsUI);
+
+  const movementsUI2 = [...document.querySelectorAll('.movements_value')];
+});
