ac426ac41a93496f983ec071ba5981f7675a70eb
Megha Khakha <meghakhakha148@gmail.com>
2025-06-07 21:27:37 +0530

    advance DOM 02

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -118,35 +118,35 @@
 
 //Implementing Smooth Scorlling
 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+// const btnScrollTo = document.querySelector('.btn--scroll-to');
+// const section1 = document.querySelector('#section--1');
 
-btnScrollTo.addEventListener('click', function(e) {
-  const s1coords = section1.getBoundingClientRect();
-  console.log(s1coords);
+// btnScrollTo.addEventListener('click', function(e) {
+//   const s1coords = section1.getBoundingClientRect();
+//   console.log(s1coords);
 
-  console.log(e.target.getBoundingClientRect());
+//   console.log(e.target.getBoundingClientRect());
 
-  console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
+//   console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
 
-  console.log
-  ('height/width viewport',
-    document.documentElement.clientHeight,
-    document.documentElement.clientWidth
-  );
+//   console.log
+//   ('height/width viewport',
+//     document.documentElement.clientHeight,
+//     document.documentElement.clientWidth
+//   );
 
-  // Scrolling
-    window.scrollTo(
-     s1coords.left + window.pageXOffset,
-     s1coords.top + window.pageYOffset
-   );
+//   // Scrolling
+//     window.scrollTo(
+//      s1coords.left + window.pageXOffset,
+//      s1coords.top + window.pageYOffset
+//    );
 
-   window.scrollTo({
-     left: s1coords.left + window.pageXOffset,
-     top: s1coords.top + window.pageYOffset,
-     behavior: 'smooth',
-   });
+//    window.scrollTo({
+//      left: s1coords.left + window.pageXOffset,
+//      top: s1coords.top + window.pageYOffset,
+//      behavior: 'smooth',
+//    });
 
-  section1.scrollIntoView({ behavior: 'smooth' });
-});
+//   section1.scrollIntoView({ behavior: 'smooth' });
+// });
 
