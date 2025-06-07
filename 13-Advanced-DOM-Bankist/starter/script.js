33112ab4de9678dd28286d651b850488ae2d0a01
Megha Khakha <meghakhakha148@gmail.com>
2025-06-07 21:21:39 +0530

    advance DOM 02

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -114,3 +114,39 @@
 
 // // Don't use
 // logo.clasName = 'shivi';
+
+
+//Implementing Smooth Scorlling
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function(e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current scroll (X/Y)', window.pageXOffset, pageYOffset);
+
+  console.log
+  ('height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // Scrolling
+    window.scrollTo(
+     s1coords.left + window.pageXOffset,
+     s1coords.top + window.pageYOffset
+   );
+
+   window.scrollTo({
+     left: s1coords.left + window.pageXOffset,
+     top: s1coords.top + window.pageYOffset,
+     behavior: 'smooth',
+   });
+
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
