0321b09b343207dd49e91799c9f5af1fefa92f92
Megha Khakha <meghakhakha148@gmail.com>
2025-06-07 21:56:01 +0530

    advance DOM 03

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -169,31 +169,31 @@
 //   alert('onmouseenter: Great! You are reading the heading :D');
 // };
 
-const h1 = document.querySelector('h1');
+// const h1 = document.querySelector('h1');
 
-//going downward: child
-console.log(h1.querySelectorAll('.highlight'));
-console.log(h1.childNodes);
-console.log(h1.children);
-h1.firstElementChild.computedStyleMap.color='white';
-h1.lastElementChild.computedStyleMap.color = 'orangered';
+// //going downward: child
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.computedStyleMap.color='white';
+// h1.lastElementChild.computedStyleMap.color = 'orangered';
 
-//going upward : parents
-console.log(h1.parentNode);
-console.log(h1.parentElement);
+// //going upward : parents
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
 
-h1.closest(.'header').style.background = 'var(--gradient-secondary)';
+// h1.closest(.'header').style.background = 'var(--gradient-secondary)';
 
-h1.closest('h1').style.background = 'var()--gradient-primary';
+// h1.closest('h1').style.background = 'var()--gradient-primary';
 
-//going sideways : siblings
-console.log(h1.previousElementSibling);
-console.log(h1.nextElementSibling);
+// //going sideways : siblings
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
 
-console.log(h1.previousSibling);
-console.log(h1.nextSibling);
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
 
-console.log(h1.parentElement.children);
-[...h1.parentElement.children].forEach(function (eg) {
-    if (el !==h1) el.style.transform = 'scale(0.5)';
-});
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(function (eg) {
+//     if (el !==h1) el.style.transform = 'scale(0.5)';
+// });
