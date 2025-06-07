cc644038049b3b1f5835795c76450da27d5551cd
Megha Khakha <meghakhakha148@gmail.com>
2025-06-07 13:45:30 +0530

    part03

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -152,26 +152,26 @@
 
 // });
 
-btnTransfer.addEventListener('click', function (e){
-  e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
-  const receiverAcc = accounts.find(
-    acc => acc.username === inputTransferTo.value
-  );
-inputTransferAmount.value = inputTransferTo = '';
-  if(amount > 0 &&
-    receiverAcc &&
-    currentAccount.balance >= amount &&
-    receiverAcc?. username != currentAccount.username
-  ){
-    // Doing the transfer
-    currentAccount.movements.push(-amount);
-    receiverAcc.movements.push(amount);
+// btnTransfer.addEventListener('click', function (e){
+//   e.preventDefault();
+//   const amount = Number(inputTransferAmount.value);
+//   const receiverAcc = accounts.find(
+//     acc => acc.username === inputTransferTo.value
+//   );
+// inputTransferAmount.value = inputTransferTo = '';
+//   if(amount > 0 &&
+//     receiverAcc &&
+//     currentAccount.balance >= amount &&
+//     receiverAcc?. username != currentAccount.username
+//   ){
+//     // Doing the transfer
+//     currentAccount.movements.push(-amount);
+//     receiverAcc.movements.push(amount);
 
-    // Update UI
-    updateUI(currentAccount);
-  }
-});
+//     // Update UI
+//     updateUI(currentAccount);
+//   }
+// });
 
 // /////////////////////////////////////////////////
 // /////////////////////////////////////////////////
