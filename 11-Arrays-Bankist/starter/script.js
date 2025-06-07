3766a348865920c222dd61aabcb114312995d141
Megha Khakha <meghakhakha148@gmail.com>
2025-06-07 15:39:25 +0530

    part04

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -547,34 +547,34 @@
 
 // console.log(movements);
 
-const arr = [1, 2, 3, 4, 5, 6, 7];
-console.log(new Array(1, 2, 3, 4, 5, 6, 7));
+// const arr = [1, 2, 3, 4, 5, 6, 7];
+// console.log(new Array(1, 2, 3, 4, 5, 6, 7));
 
-//Emprty arrays + fill method
-const x = new Array(7);
-console.log(x);
-// console.log(x.map(() => 5));
-x.fill(1,3,5);
-x.fill(1, 3, 5);
-console.log(x);
+// //Emprty arrays + fill method
+// const x = new Array(7);
+// console.log(x);
+// // console.log(x.map(() => 5));
+// x.fill(1,3,5);
+// x.fill(1, 3, 5);
+// console.log(x);
 
-arr.fill(23, 4, 6);
-console.log(arr);
+// arr.fill(23, 4, 6);
+// console.log(arr);
 
-//Array.from
-const y = Array.from({length: 7}, () => 1);
-console.log(y);
+// //Array.from
+// const y = Array.from({length: 7}, () => 1);
+// console.log(y);
 
-const z = Array.from({ length: 7},() => 1 );
-console.log(z);
+// const z = Array.from({ length: 7},() => 1 );
+// console.log(z);
 
-lookBalance.addEventListner('click', function (){
-  const movementsUI =Array.from(
-    document.querySelectorAll('.movements_value'),
-    el => Number(el.textContent.replace('',''))
-  );
+// lookBalance.addEventListner('click', function (){
+//   const movementsUI =Array.from(
+//     document.querySelectorAll('.movements_value'),
+//     el => Number(el.textContent.replace('',''))
+//   );
 
-  console.log(movementsUI);
+//   console.log(movementsUI);
 
-  const movementsUI2 = [...document.querySelectorAll('.movements_value')];
-});
+//   const movementsUI2 = [...document.querySelectorAll('.movements_value')];
+// });
