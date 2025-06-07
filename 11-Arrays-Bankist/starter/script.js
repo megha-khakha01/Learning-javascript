9e86e26466a3d6cc6f6781b8356a755041e5972d
Megha Khakha <meghakhakha148@gmail.com>
2025-06-07 15:14:43 +0530

    part04

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -500,21 +500,21 @@
 // console.log(movements.filter(deposit));
 
 
-const arr = [[ 1, 2 , 3], [4, 5 , 6], 7, 8];
-console.log(arr.flat());
+// const arr = [[ 1, 2 , 3], [4, 5 , 6], 7, 8];
+// console.log(arr.flat());
 
-const arrDeep = [[[1, 2], 3] [4,  [5, 6]], 7, 8];
-console.log(arrDeep.flat(2));
+// const arrDeep = [[[1, 2], 3] [4,  [5, 6]], 7, 8];
+// console.log(arrDeep.flat(2));
 
-// flat
-const overalBalance = accounts
-.map(acc => acc.movements)
-.flat()
-.reduce((acc, mov) => acc + mov, 0);
-console.log(overalBalance);
+// // flat
+// const overalBalance = accounts
+// .map(acc => acc.movements)
+// .flat()
+// .reduce((acc, mov) => acc + mov, 0);
+// console.log(overalBalance);
 
-// flatMap
-const overalBalance2 = accounts
-.flatMap (acc => acc.movements)
-.reduce(acc, mov)=> acc + mov, 0);
-console.log(overalBalance2);
+// // flatMap
+// const overalBalance2 = accounts
+// .flatMap (acc => acc.movements)
+// .reduce(acc, mov)=> acc + mov, 0);
+// console.log(overalBalance2);
