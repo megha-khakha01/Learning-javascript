4f362a7cf8b49be22a915cbe1d7cd5319e0efa9d
Megha Khakha <meghakhakha148@gmail.com>
2025-06-08 12:24:43 +0530

    OOPS part-01 completed

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -77,26 +77,26 @@
 
 // Coding Challenge
 
-const Car = function (make, speed) {
-    this.make = make;
-    this.speed = speed;
-}
+// const Car = function (make, speed) {
+//     this.make = make;
+//     this.speed = speed;
+// }
  
-Car.prototype.accelerate = function(){
-    this.speed += 10;
-    console.log('${this.make} is going at ${this.speed}km/h');
-}
+// Car.prototype.accelerate = function(){
+//     this.speed += 10;
+//     console.log('${this.make} is going at ${this.speed}km/h');
+// }
 
-Car.prototype.accelerate = function(){
-    this.speed -= 5;
-    console.log('${this.make} is going at ${this.speed}km/h');
-}
-const bmw = new Car ('BMW', 120);
-const mercedes = new Car ('Mercedes', 95);
+// Car.prototype.accelerate = function(){
+//     this.speed -= 5;
+//     console.log('${this.make} is going at ${this.speed}km/h');
+// }
+// const bmw = new Car ('BMW', 120);
+// const mercedes = new Car ('Mercedes', 95);
 
 
-bmw.accelerate();
-bmw.accelerate();
-bmw.brake();
-bmw.accelerate();
+// bmw.accelerate();
+// bmw.accelerate();
+// bmw.brake();
+// bmw.accelerate();
 
