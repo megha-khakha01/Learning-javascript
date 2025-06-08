77685802436cf818eb77d7dd6b02807683c9f2d7
Megha Khakha <meghakhakha148@gmail.com>
2025-06-08 19:42:10 +0530

    OOPS part-03

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -224,40 +224,40 @@
 
 // Coding Challenge
 
-const Car = function (make , speed){
-    this.make = make;
-    this.speed = speed;
-};
+// const Car = function (make , speed){
+//     this.make = make;
+//     this.speed = speed;
+// };
 
-Car.prototype.accelerate = function(){
-    this.speed += 10;
-    console.log('${this.make} is going at ${this.speed} km/h');
-};
+// Car.prototype.accelerate = function(){
+//     this.speed += 10;
+//     console.log('${this.make} is going at ${this.speed} km/h');
+// };
 
-Car.prototype.accelerate = function(){
-    this.speed -=5 ;
-    console.log('${this.make} is going at ${this.speed} km/h');
-};
-const EV = function(make, speed, charge) {
-    Car.call(this, make, speed)
-    this.speed = charge;
-}
+// Car.prototype.accelerate = function(){
+//     this.speed -=5 ;
+//     console.log('${this.make} is going at ${this.speed} km/h');
+// };
+// const EV = function(make, speed, charge) {
+//     Car.call(this, make, speed)
+//     this.speed = charge;
+// }
 
-// Link the prototypes
-EV.prototype =  Object.create(Car.prototype);
+// // Link the prototypes
+// EV.prototype =  Object.create(Car.prototype);
 
-EV.prototype.chargeBattery = function (chargeTo) {
-    this.charge = chargeTo;
-};
+// EV.prototype.chargeBattery = function (chargeTo) {
+//     this.charge = chargeTo;
+// };
 
-EV.prototype.accelerate = function() {
-    this.speed +=20;
-    this.charge--;
-    console.log('${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}');
-};
+// EV.prototype.accelerate = function() {
+//     this.speed +=20;
+//     this.charge--;
+//     console.log('${this.make} is going at ${this.speed} km/h, with a charge of ${this.charge}');
+// };
 
-const tesla = new EV('Tesla', 120, 23);
-tesla.chargeBattery(90);
-console.log(tesla);
-tesla.brake();
-tesla.accelerate();
+// const tesla = new EV('Tesla', 120, 23);
+// tesla.chargeBattery(90);
+// console.log(tesla);
+// tesla.brake();
+// tesla.accelerate();
