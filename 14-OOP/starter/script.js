f0c351542d0934951badf679542e96f85e0880cc
Megha Khakha <meghakhakha148@gmail.com>
2025-06-08 12:38:02 +0530

    OOPS part-02

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -100,3 +100,39 @@
 // bmw.brake();
 // bmw.accelerate();
 
+
+// ES6 Classes
+
+// class expression
+// const PersonCl = class {}
+
+//class declaration
+class PersonCl {
+    constructor(firstName, birthYear){
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    }
+// Methods will be added to .prototype property
+    calcAge(){
+        console.log(2037 - this.birthYear);
+    }
+}
+greet(){
+    console.log('Hey ${this.firstName}');
+}
+
+
+const jessica = new PersonCl('Jessica', 1996);
+console.log(jessica);
+jessica.calcAge();
+
+console.log(jessica._proto_=== PersonCl.prototype);
+
+//PersonCl.prototype.greet = function () {
+  //  console.log('Hey ${this.firstName}');
+}
+jessica.greet();
+
+// 1. Classses are NOT hoisted
+// 2. Classes are first - class citizes
+// 3. Classes are executed in strict mode
