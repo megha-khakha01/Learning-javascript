7ca733dc276367eee70a5fa3cdfbed05572b0d26
Megha Khakha <meghakhakha148@gmail.com>
2025-06-08 12:23:18 +0530

    OOPS part-01 completed

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -74,3 +74,25 @@
 // const h1 = document.querySelector('h1');
 // console.dir(x => x + 1);
 
+
+// Coding Challenge
+
+const Car = function (make, speed) {
+    this.make = make;
+    this.speed = speed;
+}
+ 
+Car.prototype.accelerate = function(){
+    this.speed += 10;
+    console.log('${this.make} is going at ${this.speed}km/h');
+}
+
+const bmw = new Car ('BMW', 120);
+const mercedes = new Car ('Mercedes', 95);
+
+
+bmw.accelerate();
+bmw.accelerate();
+bmw.brake();
+bmw.accelerate();
+
