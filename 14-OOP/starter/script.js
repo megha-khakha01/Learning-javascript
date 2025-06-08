97daaef5dbbe1d020b71510f4e76bfbdbb36aaf3
Megha Khakha <meghakhakha148@gmail.com>
2025-06-08 12:24:24 +0530

    OOPS part-01 completed

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -87,6 +87,10 @@ Car.prototype.accelerate = function(){
     console.log('${this.make} is going at ${this.speed}km/h');
 }
 
+Car.prototype.accelerate = function(){
+    this.speed -= 5;
+    console.log('${this.make} is going at ${this.speed}km/h');
+}
 const bmw = new Car ('BMW', 120);
 const mercedes = new Car ('Mercedes', 95);
 
