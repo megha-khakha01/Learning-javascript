ad399155639c6c84ea776c534eac0d79c47a2eee
Megha Khakha <meghakhakha148@gmail.com>
2025-06-08 13:25:46 +0530

    OOPS part-02

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -189,35 +189,35 @@
 
 // Coding Challenge
 
-class CarCl {
-    constructor(make, speed) {
-        this.make = make;
-        this.speed = speed;
-    }
+// class CarCl {
+//     constructor(make, speed) {
+//         this.make = make;
+//         this.speed = speed;
+//     }
 
-    accelerate() {
-        this.speed += 10 ;
-        console.log('{this.make} is going out ${this.speed} km/h');
-    }
+//     accelerate() {
+//         this.speed += 10 ;
+//         console.log('{this.make} is going out ${this.speed} km/h');
+//     }
 
-    brake(){
-        this.speed -= 5;
-        console.log('{this.make} is going at ${this.speed} km/h');
-    }
+//     brake(){
+//         this.speed -= 5;
+//         console.log('{this.make} is going at ${this.speed} km/h');
+//     }
 
-    get speedUS() {
-        return this.speed / 1.6;
-    }
+//     get speedUS() {
+//         return this.speed / 1.6;
+//     }
 
-    set speedUS(speed) {
-        this.speed = speed * 1.6;
-    }
-}
+//     set speedUS(speed) {
+//         this.speed = speed * 1.6;
+//     }
+// }
 
-const ford = new CartCl('Ford', 120);
-console.log(ford.speedUS);
-ford.accelerate();
-ford.accelerate();
-ford.brake();
-ford.speedUS = 50;
-console.log(ford);
+// const ford = new CartCl('Ford', 120);
+// console.log(ford.speedUS);
+// ford.accelerate();
+// ford.accelerate();
+// ford.brake();
+// ford.speedUS = 50;
+// console.log(ford);
