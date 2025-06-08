762e7485e51e04e85ea400015647525306760a9c
Megha Khakha <meghakhakha148@gmail.com>
2025-06-08 13:25:16 +0530

    OOPS part-02

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -185,3 +185,39 @@
 // const sarah = Object.create(PersonProto);
 // sarah.init('Sarah', 1979);
 // sarah.calcAge();
+
+
+// Coding Challenge
+
+class CarCl {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate() {
+        this.speed += 10 ;
+        console.log('{this.make} is going out ${this.speed} km/h');
+    }
+
+    brake(){
+        this.speed -= 5;
+        console.log('{this.make} is going at ${this.speed} km/h');
+    }
+
+    get speedUS() {
+        return this.speed / 1.6;
+    }
+
+    set speedUS(speed) {
+        this.speed = speed * 1.6;
+    }
+}
+
+const ford = new CartCl('Ford', 120);
+console.log(ford.speedUS);
+ford.accelerate();
+ford.accelerate();
+ford.brake();
+ford.speedUS = 50;
+console.log(ford);
