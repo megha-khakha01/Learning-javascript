7a786c77a8bb01d651a5b539552c5a2226a97f7e
Megha Khakha <meghakhakha148@gmail.com>
2025-06-08 20:27:22 +0530

    OOPS part-03

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -354,24 +354,24 @@
 //     }
 
 //4. Private methods
-#approveLoan(val {
-return true;
+// #approveLoan(val {
+// return true;
 
-}
-)
-// const acc1 = new Account('Jonas', 'EUR', 1111);
+// }
+// )
+// // const acc1 = new Account('Jonas', 'EUR', 1111);
 
-// // acc1.movements.push(250);
-// // acc1.movements.push(-140);
-//acc1.approvedLoan(1000);
-acc1.deposit(250);
-acc1.withdraw(140);
-acc1.requestLoan(1000);
+// // // acc1.movements.push(250);
+// // // acc1.movements.push(-140);
+// //acc1.approvedLoan(1000);
+// acc1.deposit(250);
+// acc1.withdraw(140);
+// acc1.requestLoan(1000);
 
 
 
-console.log(acc1.#movements);
-console.log(acc1.#pin);
-console.log(acc1.#approveLoan(100));
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100));
 
-Account.helper();
+// Account.helper();
