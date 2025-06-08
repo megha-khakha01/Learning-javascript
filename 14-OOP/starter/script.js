3fdf29fec27ce8ded8af787f8380016883013634
Megha Khakha <meghakhakha148@gmail.com>
2025-06-08 20:05:31 +0530

    OOPS part-03

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -296,30 +296,30 @@
 // martha.calcAge();
 // Inheritance Between "Classes": Object.create
 
-const PersonProto ={
-    calcAge() {
-        console.log (2037 - this.birthYear);
-    },
+// const PersonProto ={
+//     calcAge() {
+//         console.log (2037 - this.birthYear);
+//     },
 
-    init(firstName, birthYear) {
-        this.firstName = firstName;
-        this.birthYear = birthYear;
-    },
-};
+//     init(firstName, birthYear) {
+//         this.firstName = firstName;
+//         this.birthYear = birthYear;
+//     },
+// };
 
-const steven = Object.create(PersonProto);
+// const steven = Object.create(PersonProto);
 
-const StudentProto = Object.create(PersonProto);
-StudentProto.init = function (firstName, birthYear, course) {
-    PersonProto.init.call(this, firstName, birthYear);
-    this.course = course;
-};
+// const StudentProto = Object.create(PersonProto);
+// StudentProto.init = function (firstName, birthYear, course) {
+//     PersonProto.init.call(this, firstName, birthYear);
+//     this.course = course;
+// };
 
-StudentProto.introdeuce = function (){
-    console.log('My name is ${this.fulName} and I study ${this.course}');
-};
+// StudentProto.introdeuce = function (){
+//     console.log('My name is ${this.fulName} and I study ${this.course}');
+// };
 
-const jay = Object.create(StudentProto);
-jay.init('Jay', 2010, 'Computer Science');
-jay.introdeuce();
-jay.calcAge();
+// const jay = Object.create(StudentProto);
+// jay.init('Jay', 2010, 'Computer Science');
+// jay.introdeuce();
+// jay.calcAge();
