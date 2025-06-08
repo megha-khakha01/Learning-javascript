e53b8adadc28b8fb5b31ad2bfde2f4dace326c57
Megha Khakha <meghakhakha148@gmail.com>
2025-06-08 12:38:48 +0530

    OOPS part-02

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -107,32 +107,32 @@
 // const PersonCl = class {}
 
 //class declaration
-class PersonCl {
-    constructor(firstName, birthYear){
-        this.firstName = firstName;
-        this.birthYear = birthYear;
-    }
-// Methods will be added to .prototype property
-    calcAge(){
-        console.log(2037 - this.birthYear);
-    }
-}
-greet(){
-    console.log('Hey ${this.firstName}');
-}
+// class PersonCl {
+//     constructor(firstName, birthYear){
+//         this.firstName = firstName;
+//         this.birthYear = birthYear;
+//     }
+// // Methods will be added to .prototype property
+//     calcAge(){
+//         console.log(2037 - this.birthYear);
+//     }
+// }
+// greet(){
+//     console.log('Hey ${this.firstName}');
+// }
 
 
-const jessica = new PersonCl('Jessica', 1996);
-console.log(jessica);
-jessica.calcAge();
+// const jessica = new PersonCl('Jessica', 1996);
+// console.log(jessica);
+// jessica.calcAge();
 
-console.log(jessica._proto_=== PersonCl.prototype);
+// console.log(jessica._proto_=== PersonCl.prototype);
 
-//PersonCl.prototype.greet = function () {
-  //  console.log('Hey ${this.firstName}');
-}
-jessica.greet();
+// //PersonCl.prototype.greet = function () {
+//   //  console.log('Hey ${this.firstName}');
+// }
+// jessica.greet();
 
-// 1. Classses are NOT hoisted
-// 2. Classes are first - class citizes
+// // 1. Classses are NOT hoisted
+// // 2. Classes are first - class citizes
 // 3. Classes are executed in strict mode
