f57f6a150a05843a052af3a8e57537acd8be4a52
Megha Khakha <meghakhakha148@gmail.com>
2025-06-09 20:29:08 +0530

    prt01

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -19,14 +19,16 @@ if(navigator.geolocation)
     const {longitude} = position.coords;
     console.log(https://www.google.pt/maps/@{latitude},${longitude}');
 
-        const map = L.map('map').setView([51.505, -0.09], 13);
+        const coords = [latitude , longitude]
 
-        L.titleLayer('http://{s}.title.openstreetmap.org/{z}/{x}/{y}.png', {
+        const map = L.map('map').setView(coords, 13);
+
+        L.titleLayer('http://{s}.title.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution:
             '&copy; <a href ="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         }) .addTo(map);
 
-        L.marker([51.5, -0.09])
+        L.marker(coords)
         .addTo(map)
         .bindPopup('A pretty CSS# popoup.<br> Easily customizable')
         .openPopup();
