dc481eec3cace5f61e2600e492ec3957060c42d4
Megha Khakha <meghakhakha148@gmail.com>
2025-06-09 20:29:29 +0530

    prt01

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -13,26 +13,26 @@
 
 
 
-if(navigator.geolocation)
-    navigator.geolocation.getCurrentPosition(function(position) {
-    const {latitude} = position.coords;
-    const {longitude} = position.coords;
-    console.log(https://www.google.pt/maps/@{latitude},${longitude}');
+// if(navigator.geolocation)
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//     const {latitude} = position.coords;
+//     const {longitude} = position.coords;
+//     console.log(https://www.google.pt/maps/@{latitude},${longitude}');
 
-        const coords = [latitude , longitude]
+//         const coords = [latitude , longitude]
 
-        const map = L.map('map').setView(coords, 13);
+//         const map = L.map('map').setView(coords, 13);
 
-        L.titleLayer('http://{s}.title.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-            attribution:
-            '&copy; <a href ="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-        }) .addTo(map);
+//         L.titleLayer('http://{s}.title.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+//             attribution:
+//             '&copy; <a href ="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+//         }) .addTo(map);
 
-        L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS# popoup.<br> Easily customizable')
-        .openPopup();
-    },
-    function() {
-        alert('Could not get your position');
-    };
+//         L.marker(coords)
+//         .addTo(map)
+//         .bindPopup('A pretty CSS# popoup.<br> Easily customizable')
+//         .openPopup();
+//     },
+//     function() {
+//         alert('Could not get your position');
+//     };
