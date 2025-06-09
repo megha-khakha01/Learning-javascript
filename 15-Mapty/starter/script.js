5b1615147f125173fba00982394819e1413ef7db
Megha Khakha <meghakhakha148@gmail.com>
2025-06-09 20:49:22 +0530

    prt02

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -22,19 +22,20 @@ if(navigator.geolocation)
         const coords = [latitude , longitude]
 
         const map = L.map('map').setView(coords, 13);
-        console.log(map);
+        //console.log(map);
 
         L.titleLayer('http://{s}.title.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
             attribution:
             '&copy; <a href ="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         }) .addTo(map);
 
-        L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS# popoup.<br> Easily customizable')
-        .openPopup();
+        
+        map.on('click', function(mapEvent){
+            console.log(mapEvent);
+            const{lat, lng} = mapEvent.latlng;
 
-        //map.on()
+      L.marker(lat, lng) .addTo(map).bindPopup('Workout').openPopup();
+        });
     },
     function() {
         alert('Could not get your position');
