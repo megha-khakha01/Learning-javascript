3376b3f9d281ff42c5ac4fc252c54265af129ab9
Megha Khakha <meghakhakha148@gmail.com>
2025-06-11 19:42:47 +0530

    asynchronous

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -42,18 +42,7 @@ const renderCountry = function (data, className = '') {
   countriesContainer.style.opacity = 1;
 };
 
-const renderError = function (msg) {
-  countriesContainer.insertAdjacentText('beforeend', msg);
-  countriesContainer.style.opacity = 1;
-};
 
-const getJSON = function (url, errorMsg = 'Something went wrong') {
-  return fetch(url).then(response => {
-    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
-
-    return response.json();
-  });
-};
 
  
 // Our First AJAX Call: XMLHttpRequest
