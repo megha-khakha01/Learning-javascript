06282aed999cc46b34d6481857169c1fce84f6a4
Megha Khakha <meghakhakha148@gmail.com>
2025-06-11 19:26:01 +0530

    asynchronous

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -309,7 +309,7 @@ wait(1)
 
 Promise.resolve('abc').then(x => console.log(x));
 Promise.reject(new Error('Problem!')).catch(x => console.error(x));
-*/
+
 
 //----------------------------------------------------------------------------------------------------------------//
 
