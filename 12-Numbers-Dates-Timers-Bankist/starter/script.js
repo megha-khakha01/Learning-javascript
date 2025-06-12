7f221219e54679e4287d5220a39e0a7bfb609bbf
Megha Khakha <meghakhakha148@gmail.com>
2025-06-12 22:11:42 +0530

    update

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -5,31 +5,31 @@
 // BANKIST APP
 
 /////////////////////////////////////////////////
-// Data
+Data
 
-// DIFFERENT DATA! Contains movement dates, currency and locale
+//DIFFERENT DATA! Contains movement dates, currency and locale
 
-// const account1 = {
-//   owner: 'Jonas Schmedtmann',
-//   movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
-//   interestRate: 1.2, // %
-//   pin: 1111,
+const account1 = {
+  owner: 'Jonas Schmedtmann',
+  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  interestRate: 1.2, // %
+  pin: 1111,
 
-//   movementsDates: [
-//     '2019-11-18T21:31:17.178Z',
-//     '2019-12-23T07:42:02.383Z',
-//     '2020-01-28T09:15:04.904Z',
-//     '2020-04-01T10:17:24.185Z',
-//     '2020-05-08T14:11:59.604Z',
-//     '2020-05-27T17:01:17.194Z',
-//     '2020-07-11T23:36:17.929Z',
-//     '2020-07-12T10:51:36.790Z',
-//   ],
-//   currency: 'EUR',
-//   locale: 'pt-PT', // de-DE
-// };
+  movementsDates: [
+    '2019-11-18T21:31:17.178Z',
+    '2019-12-23T07:42:02.383Z',
+    '2020-01-28T09:15:04.904Z',
+    '2020-04-01T10:17:24.185Z',
+    '2020-05-08T14:11:59.604Z',
+    '2020-05-27T17:01:17.194Z',
+    '2020-07-11T23:36:17.929Z',
+    '2020-07-12T10:51:36.790Z',
+  ],
+  currency: 'EUR',
+  locale: 'pt-PT', // de-DE
+};
 
-// const account2 = {
+//const account2 = {
 //   owner: 'Jessica Davis',
 //   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
 //   interestRate: 1.5,
