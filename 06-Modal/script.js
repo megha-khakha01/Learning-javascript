11c4c91261c1b0ae59fddff8d1584821959ea816
Megha Khakha <meghakhakha148@gmail.com>
2025-06-19 23:52:26 +0530

    progress

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -27,8 +27,8 @@ overlay.addEventListener('click' , closeModal);
 document.addEventListener('keydown' , function (e) {
     console.log('e.key');
 
-    if(e.key === 'Escape' && !modal.classList.contains
+    //if(e.key === 'Escape' && !modal.classList.contains
         ('hidden') ) {
             closeModal();
         }
-    });
+  //  });
